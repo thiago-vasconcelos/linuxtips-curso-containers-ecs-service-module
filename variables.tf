96a9ea85bded83ea2dc66960bb4c7f2627bc222c
thiago-vasconcelos <thiagodpvasconcelos@gmail.com>
2024-12-31 11:38:19 -0300

    Add autoscaling

--- variables.tf
+++ variables.tf
@@ -35,3 +35,87 @@ variable "environment_variables" {
 variable "capabilities" {
   type = list(any)
 }
+
+variable "scale_type" {
+  default = null
+}
+
+variable "task_minimum" {
+  default = 3
+}
+
+variable "task_maximum" {
+  default = 10
+}
+
+### Autoscaling de CPU
+
+variable "scale_out_cpu_threshold" {
+  default = 80
+}
+
+variable "scale_out_adjustment" {
+  default = 1
+}
+
+variable "scale_out_comparison_operator" {
+  default = "GreaterThanOrEqualToThreshold"
+}
+
+variable "scale_out_statistic" {
+  default = "Average"
+}
+
+variable "scale_out_period" {
+  default = 60
+}
+
+variable "scale_out_evaluation_periods" {
+  default = 2
+}
+
+variable "scale_out_cooldown" {
+  default = 60
+}
+
+variable "scale_in_cpu_threshold" {
+  default = 30
+}
+
+variable "scale_in_adjustment" {
+  default = -1
+}
+
+variable "scale_in_comparison_operator" {
+  default = "LessThanOrEqualToThreshold"
+}
+
+variable "scale_in_statistic" {
+  default = "Average"
+}
+
+variable "scale_in_period" {
+  default = 120
+}
+
+variable "scale_in_evaluation_periods" {
+  default = 3
+}
+
+variable "scale_in_cooldown" {
+  default = 120
+}
+
+### Tracking CPU
+variable "scale_tracking_cpu" {
+  default = 80
+}
+
+### Tracking Requests
+variable "alb_arn" {
+  default = null
+}
+
+variable "scale_tracking_requests" {
+  default = 0
+}
